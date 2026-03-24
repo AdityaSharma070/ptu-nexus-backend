f18cbcb145e9b9ea7d8d3e9fcadde8c5bdacdc6b
Aditya Sharma <luffysharma0707@gmail.com>
2026-03-24 07:05:15 +0530

    fixed cors final

--- src/server.js
+++ src/server.js
@@ -9,14 +9,19 @@ require('./config/db');
 const app = express();
 
 
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://ptu-nexus.vercel.app'  
-  ],
-  credentials: true
-}));
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://ptu-nexus.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // 🔥 STOP HERE (fixes your issue)
+  }
+
+  next();
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
